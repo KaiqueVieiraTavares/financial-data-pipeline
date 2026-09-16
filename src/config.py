f838c86321567b8ff7e354f9c8cfa6740b6ed00e
KaiqueVieiraTavares <kaique23112005@gmail.com>
2026-09-16 13:01:10 -0300

    corrigindo a porta do postgresql

--- src/config.py
+++ src/config.py
@@ -1,20 +1,26 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / ".env"
+
+
+load_dotenv(dotenv_path=ENV_PATH, override=True)
 
 
 class Settings:
     API_URL: str = os.getenv("API_URL", "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-
-    DB_USER: str = os.getenv("DB_USER")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
-    DB_NAME: str = os.getenv("DB_NAME")
+    DB_USER: str = os.getenv("DB_USER", "admin")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "admin123")
+    DB_NAME: str = os.getenv("DB_NAME", "financial_db")
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
-    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_PORT: str = os.getenv("DB_PORT", "5433")
 
     @property
     def database_url(self) -> str:
-        return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
 
 settings = Settings()
